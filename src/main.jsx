827ece5a8dba5930795d633a71f615861cceb198
Grace Eunhye Yang <grace_yang@Graces-MacBook-Pro-2.local>
2026-05-24 23:31:50 +0800

    circular links

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,7 @@ import { ProjectsPage } from './pages/Projects';
 import { ProjectCard } from './components/ProjectCard';
 
 const renderPortfolio = () => {
-    const navBar = document.querySelector('#navigation');
     const projectGrid = document.querySelector('#project-grid');
-    
-    navBar.innerHTML = navigations
-        .map(navigation => Navigation(navigation))
-        .join('');
 
     // Maps through your JSON and converts it into HTML components
     projectGrid.innerHTML = projects
