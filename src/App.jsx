ebdb08fdf9c5a5336eb81ea1493890ea10fa97d9
Grace Eunhye Yang <grace_yang@Graces-MacBook-Pro-2.local>
2026-05-25 00:37:49 +0800

    router

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/about';
-import Projects from './pages/projects';
-import Publications from './pages/publications';
-import Contact from './pages/contact';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Publications from './pages/Publications';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -17,10 +17,10 @@ function App() {
           <nav className="nav-bar">
             <div className="nav-links">
               <Link to="/" className="nav-cta">Home</Link>
-              <Link to="/about" className="nav-cta">About</Link>
-              <Link to="/projects" className="nav-cta">Projects</Link>
-              <Link to="/publications" className="nav-cta">Publications</Link>
-              <Link to="/contact" className="nav-cta">Contact</Link>
+              <Link to="/About" className="nav-cta">About</Link>
+              <Link to="/Projects" className="nav-cta">Projects</Link>
+              <Link to="/Publications" className="nav-cta">Publications</Link>
+              <Link to="/Contact" className="nav-cta">Contact</Link>
             </div>
           </nav>
         </header>
@@ -28,10 +28,10 @@ function App() {
         {/* This defines which component loads based on the current browser URL path */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/publications" element={<Publications />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Publications" element={<Publications />} />
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Publications.jsx
@@ -0,0 +1,8 @@
+export default function Publications() {
+  return (
+    <div>
+      <h1>Publications Page</h1>
+      <p>This is the about page built with React!</p>
+    </div>
+  );
+}
