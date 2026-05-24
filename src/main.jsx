0d06100bf0a0966fff4f7eb2ea4d12c2c17cc77c
Grace Eunhye Yang <grace_yang@Graces-MacBook-Pro-2.local>
2026-05-25 00:32:08 +0800

    router

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './style.css'; 
 
-ReactDOM.createRoot(document.getElementById('root')).render( // <--- Must be .render()
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
