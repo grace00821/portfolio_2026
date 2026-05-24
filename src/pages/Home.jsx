02e27ab456507f34a4784f6dd233028c13c4be26
Grace Eunhye Yang <grace_yang@Graces-MacBook-Pro-2.local>
2026-05-25 00:47:07 +0800

    fix images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@
 import React from 'react';
 import projects from '../data/projects.json';
 import { ProjectCard } from '../components/ProjectCard';
+import microchipIcon from '../assets/microchip.png';
+import codeIcon from '../assets/code.png';
+import biotechnologyIcon from '../assets/biotechnology.png';
 
 export default function Home() {
   return (
@@ -19,7 +22,7 @@ export default function Home() {
             id="ai-icon" 
             style={{ left: '50%', top: 'calc(50% - 300px)' }}
           >
-            <img src="/src/assets/microchip.png" alt="Artificial Intelligence" />
+            <img src={microchipIcon} alt="Artificial Intelligence" />
             <div className="icon-text">Artificial Intelligence</div>
           </a>
           
@@ -32,7 +35,7 @@ export default function Home() {
             id="code-icon" 
             style={{ left: 'calc(50% + (300px * 0.866))', top: 'calc(50% + (300px * 0.5))' }}
           >
-            <img src="/src/assets/code.png" alt="Software Engineering" />
+            <img src={codeIcon} alt="Software Engineering" />
             <div className="icon-text">Software Engineering</div>
           </a>
           
@@ -45,7 +48,7 @@ export default function Home() {
             id="lab-icon" 
             style={{ left: 'calc(50% - (300px * 0.866))', top: 'calc(50% + (300px * 0.5))' }}
           >
-            <img src="/src/assets/biotechnology.png" alt="Biomedical Sciences" />
+            <img src={biotechnologyIcon} alt="Biomedical Sciences" />
             <div className="icon-text">Biomedical Sciences</div>
           </a>
         </div>
