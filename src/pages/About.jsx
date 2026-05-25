5524e75bd0beed58333c070e595b314f5da016ea
Grace Eunhye Yang <grace_yang@Graces-MacBook-Pro-2.local>
2026-05-25 21:38:21 +0800

    about page init

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 export default function About() {
   return (
-    <main>
-      <h2 className="section-title">
-        Education
-      </h2>
-      <h2 className="section-title">
-        Work experience
-      </h2>
-    </main>
+    <div className="app-container">
+      <main>
+        <h2 className="section-title">
+          Education
+        </h2>
+        <h2 className="section-title">
+          Work experience
+        </h2>
+      </main>
+    </div>
   );
 }
