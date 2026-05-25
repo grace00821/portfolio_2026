1b076eaad9c839f2b2256469589d816f51b62255
Grace Eunhye Yang <grace_yang@Graces-MacBook-Pro-2.local>
2026-05-25 23:54:12 +0800

    more projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 // src/pages/Home.jsx
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import projects from '../data/projects.json';
-import Projects from './Projects';
+// import Projects from './Projects';
 import { ProjectCard } from '../components/ProjectCard';
 
 export default function Home() {
@@ -55,7 +55,7 @@ export default function Home() {
 
       <h2 className="section-title">
         Project Selections
-        <a href="/Projects" style={{ fontSize: '10px' }}> [More projects]</a>
+        <Link to="/Projects" style={{ fontSize: '10px' }}> [More projects]</Link>
       </h2>
 
       <section id="project-grid" className="grid">
